839ee69e37e5e85350eeb92c2dec62d7e5b460a9
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2021-02-04 14:34:02 -0300

    Added footer component.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import './global.css';
 
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
