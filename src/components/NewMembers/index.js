8a71b12932fa733e8232ed6a1a6052c1b28be234
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2021-02-22 11:25:18 -0300

    Creating a simple stylization for the new members section.

--- src/components/NewMembers/index.js
+++ src/components/NewMembers/index.js
@@ -1,8 +1,33 @@
 import React from "react";
 
+import "./styles.css";
+
 function NewMembers() {
   return (
-    <h1 className="title-panel">Últimos egressos cadastrados</h1>
+    <>
+      <h1 className="title-panel-new-members">Últimos egressos cadastrados</h1>
+
+      <div className="new-members-list">
+        <ul>
+          <li>
+            <p>Fulano</p>
+            <a href="#">Linkedln</a>
+          </li>
+          <li>
+            <p>Ciclano</p>
+            <a href="#">Linkedln</a>
+          </li>
+          <li>
+            <p>Beltrano</p>
+            <a href="#">Linkedln</a>
+          </li>
+          <li>
+            <p>Outro</p>
+            <a href="#">Linkedln</a>
+          </li>
+        </ul>
+      </div>
+    </>
   )
 }
 
