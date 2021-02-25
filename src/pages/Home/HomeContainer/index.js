58052f3143c9d89972181e354441261e855366d4
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2021-02-25 17:51:09 -0300

    Adding logic to connection of the backend in home page.

--- src/pages/Home/HomeContainer/index.js
+++ src/pages/Home/HomeContainer/index.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
 import SearchImg from "../../../assets/search.png";
+
 import Button from "../../../components/Button";
+
+import { api_AS } from "../../../services/api";
+import handleSubmit from "../../../services/login";
+
 import "./styles.css";
 
-export default function HomeContainer() {
+function HomeContainer() {
+  const [statistics, setStatistics] = useState({});
+  const [courseName, setCourseName] = useState("computing-science");
+
+  useEffect(() => {
+    handleSubmit();
+    handleStatistics();
+  }, []);
+
+  const handleStatistics = async () => {
+    let query = `alumniSiteStatistics?courseName=${ courseName }`;
+    const res = await api_AS.get(query, {
+      headers: { "Authentication-Token": localStorage.getItem("eureca-token") },
+    });
+
+    if (res.status === 200) {
+      setStatistics(res.data);
+      console.log(statistics.historyYears);
+    } else {
+      console.error("error: response error");  
+    }
+  }
+
   return (
     <div id='content'>
       <div className='info'>
@@ -29,6 +57,10 @@ export default function HomeContainer() {
             <p>alunos formados no ultimo ano</p>
           </div>
           <div id='third'>
+            <h3>{ statistics.historyYears }</h3>
+            <p>Anos de historia</p>
+          </div>
+          <div id='fourth'>
             <h3>40</h3>
             <p>anos de historia</p>
           </div>
@@ -37,3 +69,5 @@ export default function HomeContainer() {
     </div>
   );
 }
+
+export default HomeContainer;
