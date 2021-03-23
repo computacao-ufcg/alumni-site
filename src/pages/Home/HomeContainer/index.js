08c977637901630d1c8bc6a0e36cb337fa6a0624
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2021-03-23 12:43:39 -0300

    Changes on styles of the HomeContainer.

--- src/pages/Home/HomeContainer/index.js
+++ src/pages/Home/HomeContainer/index.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
 
-import SearchImg from "../../../assets/search.png";
-
 import Button from "../../../components/Button";
 
 import { api_AS } from "../../../services/api";
@@ -78,43 +76,43 @@ function HomeContainer() {
               <div id='statistics'>
                 <div id='first_item'>
                   <h3>
-                    { statistics.numberAlumniGraduatedInTheLastYear !== null ? statistics.numberAlumniGraduatedInTheLastYear : "-" }
+                    { statistics.numberAlumniGraduatedInTheLastYear }
                   </h3>
                   <p>Alunos formados no último ano</p>
                 </div>
                 <div id='second_item'>
                   <h3>
-                    { statistics.numberMatchedAlumniCourse !== null ? statistics.numberMatchedAlumniCourse : "-" }
+                    { statistics.numberMatchedAlumniCourse }
                   </h3>
                   <p>Egressos <br/> mapeados</p>
                 </div>
                 <div id='third_item'>
                   <h3>
-                    { statistics.numberAcademyEmployedCourse !== null ? statistics.numberAcademyEmployedCourse : "-" }
+                    { statistics.numberAcademyEmployedCourse }
                   </h3>
                   <p>Egressos <br/> trabalhando na <br/> academia</p>
                 </div>
                 <div id='fourth_item'>
                   <h3>
-                    { statistics.numberGovernmentEmployedCourse !== null ? statistics.numberGovernmentEmployedCourse : "-" }
+                    { statistics.numberGovernmentEmployedCourse }
                   </h3>
                   <p>Egressos <br/> trabalhando no <br/> governo</p>
                 </div>
                 <div id='fifth_item'>
                   <h3>
-                    { statistics.numberMixedCompanyEmployedCourse !== null ? statistics.numberMixedCompanyEmployedCourse : "-" }
+                    { statistics.numberMixedCompanyEmployedCourse }
                   </h3>
                   <p>Egressos <br/> trabalhando na <br/> indústria</p>
                 </div>
                 <div id='sixth_item'>
                   <h3>
-                    { statistics.numberPrivateCompanyEmployedCourse !== null ? statistics.numberPrivateCompanyEmployedCourse : "-" }
+                    { statistics.numberPrivateCompanyEmployedCourse }
                   </h3>
                   <p>Egressos trabalhando em companhias privadas</p>
                 </div>
                 <div id='seventh_item'>
                   <h3>
-                    { statistics.numberPublicCompanyEmployedCourse !== null ? statistics.numberPublicCompanyEmployedCourse : "-" }
+                    { statistics.numberPublicCompanyEmployedCourse }
                   </h3>
                   <p>Egressos trabalhando em companhias públicas</p>
                 </div>
